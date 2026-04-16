d727ae0bd94c2853d42bf300e4fa8b68edb24cfa
23mist1703-lab <23mist1703@sonline20.sen.go.kr>
2026-04-16 16:15:45 +0900

    ee

---
+++ My project (2)/Assets/Script/EnemyController.cs
@@ -0,0 +1,39 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//public class NewMonoBehaviourScript : MonoBehaviour
+
+
+    
+
+//{
+   // private void OnTriggerEnter2D(Collider2D collision)
+   // {
+      //  if (collision.CompareTag("Respawn"))
+      //  {
+       //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      //  }
+       // if (collision.CompareTag("finish"))
+       // {
+      //      collision.GetComponent,LevelObject>().MoveToNextLevel();
+       // }
+      //  if (collision.CompareTag("Enemy"))
+      //  {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+       // }
+    //}
+    
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+   // {
+        
+   // }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+//}
